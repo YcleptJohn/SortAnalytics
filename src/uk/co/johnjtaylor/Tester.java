d7a8249cc4291bf639d8b57b4da260ebfd3e8417
John <YcleptJohn@gmail.com>
2016-10-28 21:50:32 +0100

    Added array generation methods

--- src/uk/co/johnjtaylor/Tester.java
+++ src/uk/co/johnjtaylor/Tester.java
@@ -9,6 +9,15 @@ public class Tester {
 		
 	}
 	
+	public int[] genArray(int length, int min, int max) {
+		int[] result = new int[length];
+		for(int i = 0; i < length; i++) {
+			int nextNum = rand.nextInt((max-min) + 1) + min;
+			result[i] = nextNum;
+		}
+		return result;
+	}
+	
 	
 
 }
