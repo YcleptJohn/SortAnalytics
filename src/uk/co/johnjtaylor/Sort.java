30c1898a193cca178acb298356709f66dcdb3193
John <YcleptJohn@gmail.com>
2016-10-28 22:30:07 +0100

    Added abstract sortArray method for strictness
    Ensures that extension classes implement that method

--- src/uk/co/johnjtaylor/Sort.java
+++ src/uk/co/johnjtaylor/Sort.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public abstract class Sort<T extends Comparable<T>> {
 	
+	abstract public T[] sortArray(T[] array);
+	
     public void printArray(T[] array) {
         System.out.println(Arrays.toString(array));
     }
