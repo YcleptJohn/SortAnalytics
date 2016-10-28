1655e0f9e8c0208f6056565fe4ebef81507b7315
John <YcleptJohn@gmail.com>
2016-10-28 23:24:03 +0100

    Handles time in a pausable manner

---
+++ src/uk/co/johnjtaylor/Time.java
@@ -0,0 +1,43 @@
+package uk.co.johnjtaylor;
+
+import java.util.TimerTask;
+import java.util.Timer;
+
+public class Time {
+	private Long elapsed;
+	private Timer scheduler = new Timer();
+	private TimerTask increment;
+	
+	public Time() {
+		elapsed = 0L;
+		
+	}
+	
+	public void start() {
+		increment = new TimerTask() {
+			@Override
+			public void run() {
+				elapsed++;
+			}
+		};
+		scheduler.scheduleAtFixedRate(increment, 0L, 1L);
+	}
+	
+	public void pause() {
+		stop();
+	}
+	
+	public void unpause() {
+		start();
+	}
+	
+	public void stop() {
+		increment.cancel();
+	}
+	
+	public Long getTime() {
+		return elapsed;
+	}
+
+
+}
