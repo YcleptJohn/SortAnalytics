08fe72d16e3f926583d405b659728caeee619f7c
John <YcleptJohn@gmail.com>
2016-10-28 21:31:33 +0100

    Created sortArray for Comparable<T>
    Any array of type T which is comparable can be sorted via this
    bubblesort's sortArray method. Although an inefficient sort it makes for
    easy implementation to begin working on analytics code.

--- src/uk/co/johnjtaylor/BubbleSort.java
+++ src/uk/co/johnjtaylor/BubbleSort.java
@@ -1,9 +1,30 @@
 package uk.co.johnjtaylor;
 
-public class BubbleSort {
+/**
+ * A Simple bubblesort class used as an easy base-line for algorithm testing
+ * @author John
+ * @param <T>
+ *
+ */
+public class BubbleSort<T extends Comparable<T>> extends Sort {
 
-	public BubbleSort() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Takes an array of any comparable type and sorts it
+	 * @param array The array the be sorted
+	 */
+	public void sortArray(T[] array) {
+	    int swaps;
+		do {
+		    swaps = 0;
+			for(int i = 0; i < array.length-1; i++) {
+				if(array[i].compareTo(array[i+1]) > 0) {
+					T temp = array[i+1];
+					array[i+1] = array[i];
+					array[i] = temp;
+					swaps++;
+				}
+			}
+		} while (swaps > 0);
 	}
 
 }
