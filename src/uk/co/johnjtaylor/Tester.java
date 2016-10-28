cdb7463ba1c424abda0b0910818d4e3fc84fd27d
John <YcleptJohn@gmail.com>
2016-10-28 22:02:47 +0100

    Included non-min/max array gen

--- src/uk/co/johnjtaylor/Tester.java
+++ src/uk/co/johnjtaylor/Tester.java
@@ -9,10 +9,30 @@ public class Tester {
 		
 	}
 	
+	/**
+	 * Generate an array of given length but only including a chosen range of numbers
+	 * @param length Length of the generated array
+	 * @param min Minimum number the integer values inside the array can have
+	 * @param max Maximum number the integer values inside the array can have
+	 */
 	public int[] genArray(int length, int min, int max) {
 		int[] result = new int[length];
 		for(int i = 0; i < length; i++) {
 			int nextNum = rand.nextInt((max-min) + 1) + min;
+			result[i] = nextNum;
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Generates an array of given length with no min/max
+	 * @param length Length of the array
+	 */
+	public int[] genArray(int length) {
+		int[] result = new int[length];
+		for(int i = 0; i < length; i++) {
+			int nextNum = rand.nextInt();
 			result[i] = nextNum;
 		}
 		return result;
