a53cb47f2bca634cb3e2e1ff50c92098264fc716
John <YcleptJohn@gmail.com>
2016-10-31 20:31:27 +0000

    parseScaleString is now a WIP
    Currently: splits into an arraylist by-operator and then runs basic
    evaluation to ensure a valid scale was given; throwing an invalid arg
    exception if necessary. Also includes an overloaded findInArray function
    which can search a specified range in an array for a character and the
    index of that character is returned. (first find only which is why there
    is a method to specify start index for easily finding a right bracket
    for a given left etc etc)

--- src/uk/co/johnjtaylor/Tester.java
+++ src/uk/co/johnjtaylor/Tester.java
@@ -1,5 +1,8 @@
 package uk.co.johnjtaylor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Tester {
@@ -9,6 +12,7 @@ public class Tester {
 	public Tester() {
 		rand = new Random();
 		timer = new Time();
+		
 	}
 	
 	/**
@@ -40,6 +44,95 @@ public class Tester {
 		return result;
 	}
 	
+	/**
+	 * Parses a scale-string, these are used to call tester's test methods with specific
+	 * input length scaling functions, e.g "n*2", "n^3", "n+5"
+	 * @param scale The scale string to parse
+	 * @param n The actual length of the array in use(used as n values in scale formulae)
+	 * @return The calculated result which is the next array length to use for testing
+	 */
+	public int parseScaleString(String scale, int n) {
+		scale = scale.trim().toLowerCase();
+		String[] scaleArray = scale.split("(?=[*+-/^()])|(?<=[*+-/^()])");
+		ArrayList<String> scalePieces = new ArrayList<String>(Arrays.asList(scaleArray));
+		
+		Iterator<String> it = scalePieces.iterator();
+		int leftBracketCount = 0;
+		int rightBracketCount = 0;
+		int numberCount = 0;
+		int operatorCount = 0;
+		int nCount = 0;
+		while(it.hasNext()) {
+			String current = it.next();
+			if(current.equals("(")) { 
+				leftBracketCount++;
+			} 
+			else if (current.equals(")")) { 
+				rightBracketCount++;
+			}
+			else if(current.matches("[0-9]*")) {
+				numberCount++;
+			}
+			else if(current.matches("[-+/*^]")) {
+				operatorCount++;
+			}
+			else if(current.equals("n")) {
+				nCount++;
+			}
+		}
+		if(leftBracketCount != rightBracketCount) {
+			throw new IllegalArgumentException("An unequal number of left and right brackets were given - they must be paired");
+		}
+		else if(nCount == 0) {
+			throw new IllegalArgumentException("Your scale contained no reference to \'n\'(the current size). This is required for a scaling operation.");
+		}
+		
+		
+		
+		
+		
+		
+		System.out.println("LB: " + leftBracketCount);
+		System.out.println("RB: " + rightBracketCount);
+		System.out.println("#: " + numberCount);
+		System.out.println("Op: " + operatorCount);
+		
+		printArrayList(scalePieces);
+		return 0;
+	}
+	
+	public void printArrayList(ArrayList<String> al) {
+		Iterator<String> it = al.iterator();
+		System.out.print("[");
+		while(it.hasNext()) {
+			String current = it.next();
+			if(it.hasNext()) {
+				System.out.print(current + ", ");
+			} else {
+				System.out.print(current);
+			}
+		}
+		System.out.print("]\n");
+	}
+	
+	/**
+	 * Searches a given array for a given character
+	 * @param array The array to be searched through
+	 * @param s The character to search for
+	 * @return The index of the character if found; Null if not found
+	 */
+	public Integer findNext(String[] array, String s) {
+		return findNext(array, s, 0);
+	}
+	
+	public Integer findNext(String[] array, String s, int startIndex) {
+		for(int i = startIndex; i < array.length; i++) {
+			if(array[i].equals(s)) {
+				return i;
+			}
+		}
+		return null;
+	}
 
 	
 	/**
@@ -67,10 +160,7 @@ public class Tester {
 	
 	public static void main(String[] args) {
 		Tester t = new Tester();
-		t.test(new BubbleSort<Integer>(), 10000, "n", 10);
+		t.parseScaleString("(n*500)^2+(30*2)", 50);
+		System.exit(0);
 	}
-	
-	
-	
-
 }
