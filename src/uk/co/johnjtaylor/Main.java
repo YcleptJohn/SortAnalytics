b1c820713bb6be619e138b299e183e5ce8d70ad9
John <YcleptJohn@gmail.com>
2017-05-30 02:13:09 +0100

    Moved void Main from Tester->Main to avoid static refs

---
+++ src/uk/co/johnjtaylor/Main.java
@@ -0,0 +1,16 @@
+package uk.co.johnjtaylor;
+
+import uk.co.johnjtaylor.arrays.QuickSort;
+
+public class Main {
+	public static void main(String[] args) {
+		Tester t = new Tester();
+		/*t.parseScaleString("(n*500)^2+(30*2)", 69);
+		System.exit(0);
+		BubbleSort<Integer> bub = new BubbleSort<>();
+		t.test(bub, 100, "ff", 5);*/
+		QuickSort<Integer> qs = new QuickSort<>();
+		t.test(qs, 1500, "2", 15);
+		System.out.println("EXEC END");
+	}
+}
