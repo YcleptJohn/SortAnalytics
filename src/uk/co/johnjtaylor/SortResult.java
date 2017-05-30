6f9a2e8355085936c51e301c1bbe16627ad7c8d7
John <YcleptJohn@gmail.com>
2017-05-30 02:11:45 +0100

    created to store the result of each sort test
    Enables moving away from simple output into real data output, interfaces
    etc

---
+++ src/uk/co/johnjtaylor/SortResult.java
@@ -0,0 +1,9 @@
+package uk.co.johnjtaylor;
+
+public class SortResult {
+	private int size;
+	private long time;
+	private String timeUnits;
+	private String dataType;
+	private String dataStructure;
+}
