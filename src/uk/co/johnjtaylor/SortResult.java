02f73f550f7292d8504fd8d4b1b28fed5cf3f1e5
John <YcleptJohn@gmail.com>
2017-06-01 00:08:33 +0100

    Created SortResult class to store results of individual sort tests

--- src/uk/co/johnjtaylor/SortResult.java
+++ src/uk/co/johnjtaylor/SortResult.java
@@ -1,9 +1,74 @@
 package uk.co.johnjtaylor;
 
 public class SortResult {
-	private int size;
 	private long time;
 	private String timeUnits;
+	private int size;
 	private String dataType;
-	private String dataStructure;
+	private DataStructure dataStructure;
+	private boolean sortCorrectness;
+	
+	/**
+	 * Initialise a new sortResult with appropriate values
+	 * @param time 
+	 * @param timeUnits
+	 * @param size Size of data input
+	 * @param dataType
+	 * @param dataStructure
+	 * @param sortCorrectness
+	 */
+	public SortResult(long time, String timeUnits, int size, String dataType, DataStructure dataStructure,
+			boolean sortCorrectness) {
+		this.time = time;
+		this.timeUnits = timeUnits;
+		this.size = size;
+		this.dataType = dataType;
+		this.dataStructure = dataStructure;
+		this.sortCorrectness = sortCorrectness;
+	}
+	
+	
+	/**
+	 * @return the time
+	 */
+	public long getTime() {
+		return time;
+	}
+	
+	/**
+	 * @return the timeUnits
+	 */
+	public String getTimeUnits() {
+		return timeUnits;
+	}
+	
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+	
+	/**
+	 * @return the dataType
+	 */
+	public String getDataType() {
+		return dataType;
+	}
+	
+	/**
+	 * @return the dataStructure
+	 */
+	public DataStructure getDataStructure() {
+		return dataStructure;
+	}
+	
+	/**
+	 * @return the sortCorrectness
+	 */
+	public boolean isSortCorrect() {
+		return sortCorrectness;
+	}
+	
+
 }
