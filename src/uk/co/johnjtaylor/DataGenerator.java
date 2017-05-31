60e16b8313c3713c5166dd0dd6fdeb52259c90fb
John <YcleptJohn@gmail.com>
2017-06-01 00:56:00 +0100

    Created a skeleton for data generation of generic data as per DataType

--- src/uk/co/johnjtaylor/DataGenerator.java
+++ src/uk/co/johnjtaylor/DataGenerator.java
@@ -1,10 +1,20 @@
 package uk.co.johnjtaylor;
+
+import uk.co.johnjtaylor.enums.DataType;
+
 /**
  * Generates data to be sorted in different DataStructures 
  * specified by the DataStructure enum
  * @author John
  *
  */
-public class DataGenerator {
-	public 
+public class DataGenerator<T extends Comparable<T>> {
+	
+	public T[] makeArray(int size, DataType type) {
+		return null;
+	}
+	
+	public T[] makeLinkedList(int size, DataType type) {
+		return null;
+	}
 }
