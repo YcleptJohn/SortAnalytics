be359e2d55de96825053d2305e278a971b4b2b1b
John <YcleptJohn@gmail.com>
2017-06-01 13:00:38 +0100

    Updating basic test cases to use new classes

--- src/uk/co/johnjtaylor/Main.java
+++ src/uk/co/johnjtaylor/Main.java
@@ -1,6 +1,10 @@
 package uk.co.johnjtaylor;
 
-import uk.co.johnjtaylor.arrays.QuickSort;
+import java.util.ArrayList;
+
+import uk.co.johnjtaylor.enums.DataStructure;
+import uk.co.johnjtaylor.enums.DataType;
+import uk.co.johnjtaylor.sorts.QuickSort;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,7 +14,22 @@ public class Main {
 		BubbleSort<Integer> bub = new BubbleSort<>();
 		t.test(bub, 100, "ff", 5);*/
 		QuickSort<Integer> qs = new QuickSort<>();
-		t.run(qs, 1500, "2", 15);
+		SortTest st = new SortTest(qs, DataStructure.ARRAY, DataType.INTEGER, 1500, "~", 15);
+		ArrayList<SortResult> results = t.run(st);
+		for(SortResult r : results) {
+			System.out.print(r.getSize());
+			System.out.print(" | " + r.getTime());
+			System.out.print(r.getTimeUnits());
+			System.out.print(" | (" + r.getDataStructure() + ")[" + r.getDataType() + "]\n");
+		}
+		
+		DataGenerator dg = new DataGenerator();
+		Integer[] array = (Integer[]) dg.makeArray(20, DataType.INTEGER);
+		for(Integer s : array) {
+			//System.out.println(s);
+		}
+		
+		
 		System.out.println("EXEC END");
 	}
 }
