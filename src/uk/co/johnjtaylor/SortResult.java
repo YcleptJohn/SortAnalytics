b93a6f98ffa06b224fc8d5d878355737d1b48a90
John <YcleptJohn@gmail.com>
2017-06-01 03:20:00 +0100

    String DataType storage -> DataType enum

--- src/uk/co/johnjtaylor/SortResult.java
+++ src/uk/co/johnjtaylor/SortResult.java
@@ -1,23 +1,26 @@
 package uk.co.johnjtaylor;
 
+import uk.co.johnjtaylor.enums.DataStructure;
+import uk.co.johnjtaylor.enums.DataType;
+
 public class SortResult {
 	private long time;
 	private String timeUnits;
 	private int size;
-	private String dataType;
+	private DataType dataType;
 	private DataStructure dataStructure;
 	private boolean sortCorrectness;
 	
 	/**
 	 * Initialise a new sortResult with appropriate values
-	 * @param time 
-	 * @param timeUnits
+	 * @param time time taken
+	 * @param timeUnits measurement unit of time
 	 * @param size Size of data input
-	 * @param dataType
-	 * @param dataStructure
-	 * @param sortCorrectness
+	 * @param dataType Type of data (enum)
+	 * @param dataStructure Structure of data (enum)
+	 * @param sortCorrectness Valid sort?
 	 */
-	public SortResult(long time, String timeUnits, int size, String dataType, DataStructure dataStructure,
+	public SortResult(long time, String timeUnits, int size, DataType dataType, DataStructure dataStructure,
 			boolean sortCorrectness) {
 		this.time = time;
 		this.timeUnits = timeUnits;
@@ -52,7 +55,7 @@ public class SortResult {
 	/**
 	 * @return the dataType
 	 */
-	public String getDataType() {
+	public DataType getDataType() {
 		return dataType;
 	}
 	
