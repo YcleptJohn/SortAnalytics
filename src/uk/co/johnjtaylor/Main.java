fb848301269e803a783e214606c373f465936ad7
John <YcleptJohn@gmail.com>
2017-06-05 01:25:26 +0100

    Committed current tests to prevent dirty branch while abroad

--- src/uk/co/johnjtaylor/Main.java
+++ src/uk/co/johnjtaylor/Main.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import uk.co.johnjtaylor.enums.DataStructure;
 import uk.co.johnjtaylor.enums.DataType;
+import uk.co.johnjtaylor.sorts.BubbleSort;
 import uk.co.johnjtaylor.sorts.QuickSort;
+import uk.co.johnjtaylor.structures.LinkedListNode;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,20 +16,37 @@ public class Main {
 		BubbleSort<Integer> bub = new BubbleSort<>();
 		t.test(bub, 100, "ff", 5);*/
 		QuickSort<Integer> qs = new QuickSort<>();
-		SortTest st = new SortTest(qs, DataStructure.ARRAY, DataType.INTEGER, 1500, "~", 15);
+		SortTest st = new SortTest(qs, DataStructure.LINKEDLIST, DataType.STRING, 5, "~", 1);
 		ArrayList<SortResult> results = t.run(st);
 		for(SortResult r : results) {
 			System.out.print(r.getSize());
 			System.out.print(" | " + r.getTime());
 			System.out.print(r.getTimeUnits());
 			System.out.print(" | (" + r.getDataStructure() + ")[" + r.getDataType() + "]\n");
+			
 		}
 		
-		DataGenerator dg = new DataGenerator();
+		
+		
+		
+		
+		DataGenerator dg = new DataGenerator();/*
 		Integer[] array = (Integer[]) dg.makeArray(20, DataType.INTEGER);
 		for(Integer s : array) {
 			//System.out.println(s);
+		}*/
+		
+		LinkedListNode<Integer> head = dg.makeLinkedList(5, DataType.INTEGER);
+		LinkedListNode<Integer> curr = head;
+		System.out.println("UNSORTED");
+		while(curr.getNextNode() != null) {
+			System.out.println(curr.getValue());
+			curr = curr.getNextNode();
 		}
+		System.out.println("~\n~\n~");
+		
+		BubbleSort<Integer> bs = new BubbleSort<>();
+		bs.sort(head);
 		
 		
 		System.out.println("EXEC END");
