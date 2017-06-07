98c854401f3afe1dfc8a2f681989fd472294aca6
John <john@johnjtaylor.co.uk>
2017-06-07 13:02:23 -0500

    Created test methods for all timer-operations

--- src/uk/co/johnjtaylor/testing/TestTime.java
+++ src/uk/co/johnjtaylor/testing/TestTime.java
@@ -1,14 +1,45 @@
 package uk.co.johnjtaylor.testing;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import uk.co.johnjtaylor.Time;
+
 public class TestTime {
-
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		assert(new Time().getTime() == 0L);
 	}
-
+	
+	@Test
+	public void testStart() throws InterruptedException {
+		Time timer = new Time();
+		timer.start();
+		Thread.sleep(20);
+		assert(timer.getTime() > 0L);
+	}
+	
+	@Test
+	public void testPause() throws InterruptedException {
+		Time timer = new Time();
+		timer.start();
+		Thread.sleep(10);
+		timer.pause();
+		Long pausedTime = timer.getTime();
+		Thread.sleep(30);
+		assert(pausedTime.equals(timer.getTime()));
+	}
+	
+	@Test
+	public void testUnpause() throws InterruptedException {
+		Time timer = new Time();
+		timer.start();
+		Thread.sleep(10);
+		timer.pause();
+		Long pausedTime = timer.getTime();
+		timer.unpause();
+		Thread.sleep(30);
+		assert(!(pausedTime.equals(timer.getTime())));
+	}
 }
