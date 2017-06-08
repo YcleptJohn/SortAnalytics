657be25c9c6e41bc98e9313ee28de0d0289bff91
John <john@johnjtaylor.co.uk>
2017-06-08 01:31:29 -0500

    Created test cases for quickSort

---
+++ src/uk/co/johnjtaylor/testing/TestQuickSort.java
@@ -0,0 +1,38 @@
+package uk.co.johnjtaylor.testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.co.johnjtaylor.DataGenerator;
+import uk.co.johnjtaylor.Tester;
+import uk.co.johnjtaylor.enums.DataType;
+import uk.co.johnjtaylor.sorts.QuickSort;
+import uk.co.johnjtaylor.structures.LinkedListNode;
+
+public class TestQuickSort<T extends Comparable<T>> {
+	private DataGenerator<T> dg;
+	private QuickSort<T> sort;
+	private Tester<T> t;
+
+	@Before
+	public void setUp() throws Exception {
+		dg = new DataGenerator<T>();
+		sort = new QuickSort<T>();
+		t = new Tester<T>();
+	}
+
+	@Test
+	public void testQuickSortArrayIntegers() {
+		 T[] intArray = dg.makeArray(50, DataType.INTEGER);
+		 Object sortResult = sort.sort(intArray);
+		 assert(t.validateSort(sortResult) == true);
+	}
+	
+	@Test
+	public void testQuickSortArrayStrings() {
+		T[] stringArray = dg.makeArray(5, DataType.STRING);
+		Object sortResult = sort.sort(stringArray);
+		assert(t.validateSort(sortResult) == true);
+	}
+
+}
