b88875843a41b565e278efba4c1d872bcccd3e9a
John <john@johnjtaylor.co.uk>
2017-06-14 00:07:16 -0500

    Created a basic implementation of the generic 'Event'

--- src/uk/co/johnjtaylor/events/generic/Event.java
+++ src/uk/co/johnjtaylor/events/generic/Event.java
@@ -1,5 +1,23 @@
 package uk.co.johnjtaylor.events.generic;
 
+import java.util.EventObject;
+
+import uk.co.johnjtaylor.events.EventType;
+
 public abstract class Event extends EventObject {
+	private static final long serialVersionUID = -8983402706390183976L;
+	private EventType type;
+
+	public Event(Object source, EventType type) {
+		super(source);
+		this.type = type;
+	}
+	
+	public EventType getEventType() {
+		return type;
+	}
+	
+	public abstract String getEventName();
+	public abstract EventImportance getEventImportance();
 	
 }
