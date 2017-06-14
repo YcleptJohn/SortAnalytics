7de1fcea3ced82c2fb45c6ec5833f122df0486a8
John <john@johnjtaylor.co.uk>
2017-06-14 00:17:17 -0500

    Forced EventImportance as an 'abstract field' so all events can be rankd

--- src/uk/co/johnjtaylor/events/generic/Event.java
+++ src/uk/co/johnjtaylor/events/generic/Event.java
@@ -2,22 +2,28 @@ package uk.co.johnjtaylor.events.generic;
 
 import java.util.EventObject;
 
+import uk.co.johnjtaylor.events.EventImportance;
 import uk.co.johnjtaylor.events.EventType;
 
 public abstract class Event extends EventObject {
 	private static final long serialVersionUID = -8983402706390183976L;
 	private EventType type;
+	private EventImportance importance;
 
-	public Event(Object source, EventType type) {
+	public Event(Object source, EventType type, EventImportance importance) {
 		super(source);
 		this.type = type;
+		this.importance = importance;
 	}
 	
 	public EventType getEventType() {
 		return type;
 	}
 	
+	public EventImportance getEventImportance() {
+		return importance;
+	}
+	
 	public abstract String getEventName();
-	public abstract EventImportance getEventImportance();
 	
 }
