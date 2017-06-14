0dc559f76160955cfa05b6fa3510bb3053660760
John <john@johnjtaylor.co.uk>
2017-06-13 23:59:57 -0500

    ListenerList class used as a representation of an ArrayList of listeners
    This is used in a HashMap within event dispatch that allows each a
    mapping of <EventType, ListenerList>; this enables multiple listeners to
    be registered to each individual event that can occur.

---
+++ src/uk/co/johnjtaylor/events/listeners/ListenerList.java
@@ -0,0 +1,7 @@
+package uk.co.johnjtaylor.events.listeners;
+
+import java.util.ArrayList;
+
+public class ListenerList extends ArrayList<Listener> {
+	private static final long serialVersionUID = 8908010070424326635L;
+}
