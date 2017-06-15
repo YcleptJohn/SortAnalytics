37aa17421fd19e51e6e1389a1f8f98566f4a8aec
John <john@johnjtaylor.co.uk>
2017-06-14 23:39:18 -0500

    Fleshed out Sort+Test event abstraction extensions +doc

--- src/uk/co/johnjtaylor/events/generic/SortEvent.java
+++ src/uk/co/johnjtaylor/events/generic/SortEvent.java
@@ -1,5 +1,28 @@
 package uk.co.johnjtaylor.events.generic;
 
+import uk.co.johnjtaylor.events.EventImportance;
+import uk.co.johnjtaylor.events.EventType;
+/**
+ * Extended representation of a SortEvent;
+ * Still presented as an abstract class as all appropriate
+ * sort-based events will implement these abstractions in 
+ * their own class definitions. 
+ * @author John
+ */
 public abstract class SortEvent extends Event {
+	private static final long serialVersionUID = -6134362372222328200L;
 
+	/**
+	 * @param source Source creating this event (EventObject constr.)
+	 * @param type EventType (enum) of the event instance
+	 * @param importance EventImportance (enum) of the event significance
+	 */
+	public SortEvent(Object source, EventType type, EventImportance importance) {
+		super(source, type, importance);
+	}
+
+	/**
+	 * @return A string representing the name of this event instance
+	 */
+	public abstract String getEventName();
 }
--- src/uk/co/johnjtaylor/events/generic/TesterEvent.java
+++ src/uk/co/johnjtaylor/events/generic/TesterEvent.java
@@ -1,5 +1,29 @@
 package uk.co.johnjtaylor.events.generic;
 
+import uk.co.johnjtaylor.events.EventImportance;
+import uk.co.johnjtaylor.events.EventType;
+/**
+ * Extended representation of a Tester-Event;
+ * Still presented as an abstract class as all appropriate
+ * test-based events will implement these abstractions in 
+ * their own class definitions. 
+ * @author John
+ */
 public abstract class TesterEvent extends Event {
+	private static final long serialVersionUID = -2166632761580844788L;
+
+	/**
+	 * @param source Source creating this event (EventObject constr.)
+	 * @param type EventType (enum) Representing which specific event occurred
+	 * @param importance EventImportance (enum) representing the significance of this event (class constant)
+	 */
+	public TesterEvent(Object source, EventType type, EventImportance importance) {
+		super(source, type, importance);
+	}
+	
+	/**
+	 * @return A string representing the name of this event instance
+	 */
+	public abstract String getEventName();
 
 }
