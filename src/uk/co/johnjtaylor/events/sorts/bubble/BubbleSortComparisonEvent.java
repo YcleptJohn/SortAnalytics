1b72190b693122fd2b1abad7d1a0b27061fc5765
John <john@johnjtaylor.co.uk>
2017-06-15 00:13:31 -0500

    Created a base representation of the BubbleSort's comparison event

--- src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortComparisonEvent.java
+++ src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortComparisonEvent.java
@@ -1,5 +1,24 @@
 package uk.co.johnjtaylor.events.sorts.bubble;
 
+import uk.co.johnjtaylor.events.EventImportance;
+import uk.co.johnjtaylor.events.EventType;
+import uk.co.johnjtaylor.events.generic.Event;
+/**
+ * BubbleSortComparison events occur whenever two values
+ * are compared via the bubble sorting algorithm. This
+ * class is a representation of those events. 
+ * @author John
+ */
 public class BubbleSortComparisonEvent extends Event {
+	private static final long serialVersionUID = -8342214094362821912L; // Enforced serializable due to EventObject extension
+	private static final EventType TYPE = EventType.BubbleSortComparisonEvent; // Type of event (super constr)
+	private static final EventImportance IMPORTANCE = EventImportance.LOWEST; //  Importance of event (super constr)
 
+	/**
+	 * Construct a new BubbleSortComparisonEvent instance
+	 * @param source The source that is spawning the event (EventObject super constr.)
+	 */
+	public BubbleSortComparisonEvent(Object source) {
+		super(source, TYPE, IMPORTANCE);
+	}
 }
