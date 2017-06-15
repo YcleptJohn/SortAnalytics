2898dc6e1fe26be28ba4f389a465a092dd7cf778
John <john@johnjtaylor.co.uk>
2017-06-15 00:08:22 -0500

    Created a base for the BubbleSortEndEvent +jdoc

--- src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortEndEvent.java
+++ src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortEndEvent.java
@@ -1,7 +1,22 @@
 package uk.co.johnjtaylor.events.sorts.bubble;
 
+import uk.co.johnjtaylor.events.EventImportance;
+import uk.co.johnjtaylor.events.EventType;
 import uk.co.johnjtaylor.events.generic.SortEvent;
-
+/**
+ * Represents the end of a bubble sort across a dataset;
+ * (detected via the lack of swaps and then spawned)
+ * @author John
+ */
 public class BubbleSortEndEvent extends SortEvent {
-
+	private static final long serialVersionUID = -8427959870167493795L; // Enforced serializable due to EventObject extension
+	private static final EventType TYPE = EventType.BubbleSortEndEvent;
+	private static final EventImportance IMPORTANCE = EventImportance.MEDIUM;
+	
+	/**
+	 * @param source The source that is spawning the new instance of this event
+	 */
+	public BubbleSortEndEvent(Object source) {
+		super(source, TYPE, IMPORTANCE);
+	}
 }
