d8067fd1270aa8f6a2f1e2156f34004dc13633b5
John <john@johnjtaylor.co.uk>
2017-06-15 00:05:07 -0500

    Implemented a base BubbleSortStartEvent +jdoc

--- src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortStartEvent.java
+++ src/uk/co/johnjtaylor/events/sorts/bubble/BubbleSortStartEvent.java
@@ -3,20 +3,20 @@ package uk.co.johnjtaylor.events.sorts.bubble;
 import uk.co.johnjtaylor.events.EventImportance;
 import uk.co.johnjtaylor.events.EventType;
 import uk.co.johnjtaylor.events.generic.SortEvent;
-
+/**
+ * Represents the start of a new bubble sort
+ * @author John
+ */
 public class BubbleSortStartEvent extends SortEvent {
 	private static final long serialVersionUID = -4600749413952297462L;
-	private final EventImportance IMPORTANCE = EventImportance.MEDIUM;
-
-	public BubbleSortStartEvent(Object source, EventType type, EventImportance importance) {
-		super(source, type, importance);
-		// TODO Auto-generated constructor stub
+	private static final EventType TYPE = EventType.BubbleSortStartEvent;
+	private static final EventImportance IMPORTANCE = EventImportance.MEDIUM;
+	
+	/**
+	 * Spawn a new BubbleSortStartEvent instance
+	 * @param source The source that is spawning the event (EventObject super constr.)
+	 */
+	public BubbleSortStartEvent(Object source) {
+		super(source, TYPE, IMPORTANCE);
 	}
-
-	@Override
-	public String getEventName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
