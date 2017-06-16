6d4f3c54426824c3e2248d514f6994ab1213977b
ycleptjohn <jjt24@kent.ac.uk>
2017-06-16 14:00:31 -0500

    Prepared eventListener list storage, constructor +jdoc

--- src/uk/co/johnjtaylor/events/EventDispatcher.java
+++ src/uk/co/johnjtaylor/events/EventDispatcher.java
@@ -1,5 +1,28 @@
 package uk.co.johnjtaylor.events;
 
+import java.util.HashMap;
+
+import uk.co.johnjtaylor.events.listeners.ListenerList;
+/**
+ * Handles Listener registration, de-registration and
+ * the dispatching of events
+ * @author John
+ *
+ */
 public class EventDispatcher {
+	private HashMap<EventType, ListenerList> eventListeners = new HashMap<>();
+	
+	/**
+	 * EventDispatcher Constructor;
+	 * Populates the eventListeners HashMap with every possible
+	 * event having an empty list of listeners (ListenerList) 
+	 * associated with it. 
+	 */
+	public EventDispatcher() {
+		for(EventType evtType : EventType.values()) {
+			eventListeners.put(evtType, new ListenerList());
+		}
+	}
+	
 	
 }
