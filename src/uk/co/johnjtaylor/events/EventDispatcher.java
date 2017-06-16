ceae6e933786d190f79d34bef712c3d5be6bf5aa
ycleptjohn <jjt24@kent.ac.uk>
2017-06-16 14:09:45 -0500

    +registerListener() - register 0+ listeners to a given event type

--- src/uk/co/johnjtaylor/events/EventDispatcher.java
+++ src/uk/co/johnjtaylor/events/EventDispatcher.java
@@ -2,6 +2,7 @@ package uk.co.johnjtaylor.events;
 
 import java.util.HashMap;
 
+import uk.co.johnjtaylor.events.listeners.Listener;
 import uk.co.johnjtaylor.events.listeners.ListenerList;
 /**
  * Handles Listener registration, de-registration and
@@ -24,5 +25,15 @@ public class EventDispatcher {
 		}
 	}
 	
-	
+	/**
+	 * Registers a new listener to a particular type of event
+	 * @param event EventType(enum) - The given type of event to attach this listener to
+	 * @param listener The event handler/listener to be attached to the given event type
+	 */
+	public synchronized void registerListener(EventType event, Listener listener) {
+		ListenerList listeners = eventListeners.get(event);
+		listeners.add(listener); // Update current ListenerList
+		eventListeners.remove(event);
+		eventListeners.put(event, listeners);
+	}
 }
