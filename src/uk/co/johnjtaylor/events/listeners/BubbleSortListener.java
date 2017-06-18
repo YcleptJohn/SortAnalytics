7cf63a217ede1e44366c778460ed780340082f6c
ycleptjohn <jjt24@kent.ac.uk>
2017-06-18 15:13:59 -0500

    Populated interface methods of BubbleSortListener

--- src/uk/co/johnjtaylor/events/listeners/BubbleSortListener.java
+++ src/uk/co/johnjtaylor/events/listeners/BubbleSortListener.java
@@ -1,5 +1,10 @@
 package uk.co.johnjtaylor.events.listeners;
 
-public interface BubbleSortListener extends Listener {
+import uk.co.johnjtaylor.events.sorts.bubble.*;
 
+public interface BubbleSortListener extends Listener {
+	public void onStart(BubbleSortStartEvent e);
+	public void onComparison(BubbleSortComparisonEvent e);
+	public void onSwap(BubbleSortSwapEvent e);
+	public void onFinish(BubbleSortEndEvent e);
 }
