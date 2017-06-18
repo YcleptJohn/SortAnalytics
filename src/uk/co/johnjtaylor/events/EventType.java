5f03b469c6d71a9d2068bcc0db940b8f87432f36
ycleptjohn <jjt24@kent.ac.uk>
2017-06-18 15:22:29 -0500

    Fleshed out Invoke() method of BubbleSort types

--- src/uk/co/johnjtaylor/events/EventType.java
+++ src/uk/co/johnjtaylor/events/EventType.java
@@ -1,9 +1,34 @@
 package uk.co.johnjtaylor.events;
 
+import uk.co.johnjtaylor.events.generic.*;
+import uk.co.johnjtaylor.events.sorts.bubble.*;
+import uk.co.johnjtaylor.events.listeners.*;
+
 public enum EventType {
-	BubbleSortStartEvent,
-	BubbleSortComparisonEvent,
-	BubbleSortSwapEvent,
-	BubbleSortEndEvent,
-	Temporary
+	BubbleSortStartEvent {
+		@Override
+		public void invoke(Event e, Listener l) {
+			((BubbleSortListener) l).onStart((BubbleSortStartEvent) e);
+		}
+	},
+	BubbleSortComparisonEvent {
+		@Override
+		public void invoke(Event e, Listener l) {
+			((BubbleSortListener) l).onComparison((BubbleSortComparisonEvent) e);
+		}
+	},
+	BubbleSortSwapEvent {
+		@Override
+		public void invoke(Event e, Listener l) {
+			((BubbleSortListener) l).onSwap((BubbleSortSwapEvent) e);
+		}
+	},
+	BubbleSortEndEvent {
+		@Override
+		public void invoke(Event e, Listener l) {
+			((BubbleSortListener) l).onFinish((BubbleSortEndEvent) e);
+		}
+	};
+	
+	public abstract void invoke(Event e, Listener l);
 }
