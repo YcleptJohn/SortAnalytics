afdb062de83b6097c1605c1fd9433be0d81d7639
ycleptjohn <jjt24@kent.ac.uk>
2017-06-18 15:58:24 -0500

    Re-defined abstract sort() to take a reference to a Timer object
    This allows the sort to handle it's own time; so it can pause to
    dispatch events without affecting the efficiency test

--- src/uk/co/johnjtaylor/Sort.java
+++ src/uk/co/johnjtaylor/Sort.java
@@ -2,6 +2,6 @@ package uk.co.johnjtaylor;
 
 public abstract class Sort<T extends Comparable<T>> {
 	
-	abstract public Object sort(Object inputData);
+	abstract public Object sort(Object inputData, Time timer);
 	
 }
