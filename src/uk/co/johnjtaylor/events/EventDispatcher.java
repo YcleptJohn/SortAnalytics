d7ef25683832e9589bbf89984a000cace0ff7d68
ycleptjohn <jjt24@kent.ac.uk>
2017-06-20 04:53:08 -0500

    Made adjustments to queue events and dispatch async(working on issue #1)

--- src/uk/co/johnjtaylor/events/EventDispatcher.java
+++ src/uk/co/johnjtaylor/events/EventDispatcher.java
@@ -1,6 +1,10 @@
 package uk.co.johnjtaylor.events;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import uk.co.johnjtaylor.events.generic.Event;
 import uk.co.johnjtaylor.events.listeners.Listener;
@@ -13,6 +17,7 @@ import uk.co.johnjtaylor.events.listeners.ListenerList;
  */
 public class EventDispatcher {
 	private static HashMap<EventType, ListenerList> eventListeners = new HashMap<>();
+	private static Queue<Event> eventQueue = new LinkedList<Event>();
 	
 	/**
 	 * EventDispatcher eventListener list construction;
@@ -24,6 +29,7 @@ public class EventDispatcher {
 		for(EventType evtType : EventType.values()) {
 			eventListeners.put(evtType, new ListenerList());
 		}
+		//startDispatchingQueuedEvents();
 	}
 	
 	/**
@@ -31,7 +37,7 @@ public class EventDispatcher {
 	 * @param event EventType(enum) - The given type of event to attach this listener to
 	 * @param listener The event handler/listener to be attached to the given event type
 	 */
-	public static synchronized void registerListener(EventType event, Listener listener) {
+	public static void registerListener(EventType event, Listener listener) {
 		ListenerList currentlisteners = eventListeners.get(event);
 		currentlisteners.add(listener); // Update current ListenerList
 		eventListeners.remove(event);
@@ -43,24 +49,42 @@ public class EventDispatcher {
 	 * @param event The event that the target listener is attached to
 	 * @param listener The target listener to be removed from this event
 	 */
-	public synchronized void unregisterListener(EventType event, Listener listener) {
+	public static void unregisterListener(EventType event, Listener listener) {
 		ListenerList currentListeners = eventListeners.get(event);
 		currentListeners.remove(listener);
 		eventListeners.remove(event);
 		eventListeners.put(event, currentListeners);
 	}
 	
-	public static synchronized void requestInvoke(Event e) {
-		dispatch(e);
+	public static void requestInvoke(Event e) {
+		eventQueue.add(e);
 	}
 	
 	/**
 	 * Handles the dispatching of events to all appropriate listeners
 	 * @param event the event to dispatch
+	 * @deprecated replaced by startDisaptchingQueuedEvents and event queueing methods
 	 */
+	@Deprecated
 	private static synchronized void dispatch(Event event) {
 		for(Listener currListener : eventListeners.get(event.getEventType())) {
 			event.getEventType().invoke(event, currListener);
 		}
+	}
+	
+	private static void startDispatchingQueuedEvents() {
+		Timer scheduler = new Timer();
+		TimerTask invokeEvents = new TimerTask() {
+			@Override
+			public void run() {
+				if(eventQueue.peek() != null) {
+					Event targetEvent = eventQueue.poll();
+					for(Listener currListener : eventListeners.get(targetEvent.getEventType())) {
+						targetEvent.getEventType().invoke(targetEvent, currListener);
+					}
+				}
+			}
+		};
+		scheduler.scheduleAtFixedRate(invokeEvents, 0L, 000000000000000000000000000001L);
 	}
 }
